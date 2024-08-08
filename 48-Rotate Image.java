b962240cf817ab759163ca1e7bb74908948817b3
SutharMahendra <omajimahendra@gmail.com>
2024-08-08 20:31:42 +0530

    Rotate Image

--- 48-Rotate Image.java
+++ 48-Rotate Image.java
@@ -1,3 +1,14 @@
+//You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+/* this is origanal arr
+  | 1,2,3 |
+  | 4,5,6 |
+  | 7,8,9 |
+  we make it like this
+  | 7,4,1 |
+  | 8,5,2 |
+  | 9,6,3 |
+
+ */
 class Solution {
     public void rotate(int[][] matrix) {
 
