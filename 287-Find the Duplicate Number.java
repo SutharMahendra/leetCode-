26aa63aafa798f2121f9b79817049b44c0f39977
SutharMahendra <omajimahendra@gmail.com>
2024-08-09 10:08:32 +0530

    Find the Duplicate Number

--- 287-Find the Duplicate Number.java
+++ 287-Find the Duplicate Number.java
@@ -1,12 +1,15 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
+
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return nums[i];
+            int element = nums[i];
+            if (map.containsKey(element)) {
+                return element;
             }
+            map.put(element, i);
         }
 
         return 0;
