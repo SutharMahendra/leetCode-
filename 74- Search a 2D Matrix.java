f3af15b985dc545d10379638a7308fadd8dc8611
SutharMahendra <omajimahendra@gmail.com>
2024-08-10 10:13:33 +0530

    Search a 2D Matrix

--- 74- Search a 2D Matrix.java
+++ 74- Search a 2D Matrix.java
@@ -1,3 +1,19 @@
+/*
+ * You are given an m x n integer matrix matrix with the following two properties:
+
+  1.Each row is sorted in non-decreasing order.
+  2.The first integer of each row is greater than the last integer of the previous row.
+
+  Given an integer target, return true if target is in matrix or false otherwise.
+
+  Example 1:
+
+  Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+  Output: true
+
+  use binary search for every row 
+ */
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
