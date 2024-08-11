a845a9d9cfb2b16dea664239b3cd052edbdab250
SutharMahendra <omajimahendra@gmail.com>
2024-08-11 10:00:33 +0530

    Pow(x, n)

--- 50-Pow(x, n).java
+++ 50-Pow(x, n).java
@@ -1,3 +1,13 @@
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+/*
+ * Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+here we use recursion method to solve this problem
+
+ */
 class Solution {
     public double myPow(double x, int n) {
         if (x == 1 || n == 0) {
