bbee4469b32fdc6e8c3d9a2e17f23d521b453d03
SutharMahendra <omajimahendra@gmail.com>
2024-08-14 09:12:42 +0530

    Two Sum

--- 74- Search a 2D Matrix.java
+++ 74- Search a 2D Matrix.java
@@ -11,7 +11,7 @@
   Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
   Output: true
 
-  use binary search for every row 
+  use binary search for every row
  */
 
 class Solution {
