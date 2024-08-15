43d62b44881572d6368b48d7da1e656c7a1f6ebf
SutharMahendra <omajimahendra@gmail.com>
2024-08-15 12:53:54 +0530

    Four Sum

--- 18-4Sum.java
+++ 18-4Sum.java
@@ -1,3 +1,13 @@
+/*
+ * Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 
+ * Example 1:
+
+    Input: nums = [1,0,-1,0,-2,2], target = 0
+    Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ */
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Set<List<Integer>> resultSet = new HashSet<>();
