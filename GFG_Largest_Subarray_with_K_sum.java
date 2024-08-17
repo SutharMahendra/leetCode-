e41b53ed5c41c3172be91933372500390c3c3292
SutharMahendra <omajimahendra@gmail.com>
2024-08-17 08:15:21 +0530

    Largest Subarray with K sum

--- GFG_Largest_Subarray_with_K_sum.java
+++ GFG_Largest_Subarray_with_K_sum.java
@@ -1,3 +1,71 @@
+/*
+ * Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+ *  
+ * Examples:
+
+    Input: arr[] = {15,-2,2,-8,1,7,10,23}, n = 8
+    Output: 5
+    Explanation: The largest subarray with sum 0 is -2 2 -8 1 7.
+
+    arr is given 
+    we take one map that contains sum as key and it's idex as value because if map contains sum that we essaly modify max_length
+
+    now take iteration 
+
+    0. sum = sum + arr[i] = 0 + 15 = 15
+       map is empty so we put in map
+       map = {15}
+    
+    1. sum = sum + arr[i] = 15 + -2 = 13
+
+       map = {15:0}
+        map can not contains 13 so put in map
+       now map = {15:0 , 13:1}
+    
+    2. sum = sum + arr[i] = 13 + 2 = 15
+
+       now map = {15:0 , 13:1}
+       map contains 15 
+       so update max_length;
+       max_length = (0 , 2-0) = 2
+
+    3. sum = sum + arr[i] = 15 + -8 = 7
+
+       
+        map = {15:0 , 13:1}
+        map can not contains 7 so put in map
+        now map = {15:0 , 13:1, 7:3}
+
+    4. sum = sum + arr[i] = 7 + 1 = 8
+
+        now map = {15:0 , 13:1, 7:3}
+        map can not contains 8 so put in map
+        now map = {15:0 , 13:1, 7:3, 8:4}
+    
+    5. sum = sum + arr[i] = 8 + 7 = 15
+
+       map = {15:0 , 13:1, 7:3, 8:4}
+        map contains 15 
+       so update max_length;
+       max_length = (2 , 5-0) = 5
+    
+    6. sum = sum + arr[i] = 15 + 10 = 25
+
+       map = {15:0 , 13:1, 7:3, 8:4}
+        map can not contains 25 so put in map
+       map = {15:0 , 13:1, 7:3, 8:4, 25:6}
+    
+    7. sum = sum + arr[i] = 25 + 23 = 48
+
+       map = {15:0 , 13:1, 7:3, 8:4, 25:6}
+        map can not contains 48 so put in map
+       map = {15:0 , 13:1, 7:3, 8:4, 25:6}
+       
+    
+    so at the end we get max_length = 5 
+
+ */
+
 import java.util.*;
 
 public class GFG_Largest_Subarray_with_K_sum {
