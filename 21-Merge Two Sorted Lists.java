61ac22964dd5ece48aa5b61ad4df23274e378928
SutharMahendra <omajimahendra@gmail.com>
2024-10-23 18:47:48 +0530

    Merge Two Sorted Lists

--- 21-Merge Two Sorted Lists.java
+++ 21-Merge Two Sorted Lists.java
@@ -1,4 +1,19 @@
 /**
+ * we simply create dummy node instead of creating new list and than just
+ * connect the node in sort
+ * two list are there
+ * list1 = [1,2,4], list2 = [1,3,4]
+ * dummy node with value -1
+ * track list1 by temp and list2 by track2 and one more listnode are there which
+ * is use to track the dummy node's connection
+ * we make comparision between temp and temp2, bigger value will be connect with
+ * the head and then move it forword
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * Definition for singly-linked list.
  * public class ListNode {
  * int val;
